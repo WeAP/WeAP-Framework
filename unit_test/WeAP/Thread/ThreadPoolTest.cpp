3d8556a2c0f055e0118e8c060a54a9a2a9602270
xzwang <270250351@qq.com>
2020-02-12 15:51:32 +0800

    test

---
+++ unit_test/WeAP/Thread/ThreadPoolTest.cpp
@@ -0,0 +1,61 @@
+#include "ThreadPoolTest.h"
+#include "DateTime.h"
+
+
+class LoopTask: public Task
+{
+public:
+    LoopTask()
+    :Task("LoopTask")
+    {
+
+    };
+    virtual ~LoopTask(){};
+
+    void Run(void* arg)
+    {
+        int* count = (int*) arg;
+        for(int i = 0; i < *count; i++)
+        {
+            cout << "loop " << i << endl;
+            sleep(1);
+        }
+    };
+
+
+};
+
+
+
+using namespace WeAP::System;
+
+ThreadPoolTest::ThreadPoolTest()
+{
+
+}
+
+ThreadPoolTest::~ThreadPoolTest()
+{
+
+}
+
+
+void ThreadPoolTest::TestRun()
+{
+    ThreadPool pool(10);
+
+    int loopCount = 20;
+
+    pool.AddTask(new LoopTask(), &loopCount);
+
+    cout << val << endl;
+
+    EXPECT_EQ(value, val);
+
+}
+
+TEST_F(ThreadPoolTest, TestRun)
+{
+    TestRun();
+}
+
---
+++ unit_test/WeAP/Thread/ThreadPoolTest.h
@@ -0,0 +1,24 @@
+#ifndef __WeAP_ThreadPoolTest_H__
+#define __WeAP_ThreadPoolTest_H__
+
+#include "ObjectTest.h"
+#include "ThreadPool.h"
+
+using namespace WeAP::Thread;
+
+class ThreadPoolTest: public ObjectTest
+{
+public:
+    ThreadPoolTest();
+    virtual ~ThreadPoolTest();
+
+    void TestRun();
+
+protected:
+
+
+};
+
+
+
+#endif //__WeAP_ThreadPoolTest_H__
